686eb8f91ce8d190fb7245ad88a808cd849ab4c7
Anya Kar <68295262+anyaKarx@users.noreply.github.com>
2022-02-20 18:32:20 +0700

    +

--- src/main/java/anyakar/externalsort/Application.java
+++ src/main/java/anyakar/externalsort/Application.java
@@ -1,10 +1,12 @@
 package anyakar.externalsort;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import anyakar.externalsort.console.ConsoleParams;
@@ -15,15 +17,31 @@ import anyakar.externalsort.merge.stack.factory.StringFileBufferFactory;
 
 public class Application {
 
-    public static <T extends Comparable<T>> void main(final String[] args) throws IOException {
+    public static <T extends Comparable<T>> void main( String[] args) throws IOException {
         Charset cs = Charset.defaultCharset();
-        ConsoleParams consoleParams = ConsoleParams.parseArgs(args);
+        Scanner in = new Scanner(System.in);
+        ConsoleParams consoleParams = null;
+                while(consoleParams == null){
+                   consoleParams = consoleParams.parseArgs(args);
+                    args = in.nextLine().split("\\s");
+                }
         List<File> files = consoleParams.getFiles().stream()
                 .map(File::new)
+                .filter(file -> file.exists())
                 .collect(Collectors.toList());
+
+         if (files.isEmpty())
+         {
+             System.out.println("Input files do not exist");
+             return;
+         }
 
         File output = new File(consoleParams.getOutputFile());
 
+         if(output.length() != 0) {
+             output.delete();
+             output = new File(consoleParams.getOutputFile());
+         }
         Comparator<T> comparator = Comparable::compareTo;
         if (consoleParams.isReverse()) {
             comparator.reversed();
